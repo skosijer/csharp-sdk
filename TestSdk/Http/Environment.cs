43628cd04da84bdae6b96d6e24cf122c926687b6
Stevan Kosijer <stkosijer@gmail.com>
2024-03-22 00:26:59 +0100

    liblab SDK update for version v1.0.1

---
+++ TestSdk/Http/Environment.cs
@@ -0,0 +1,16 @@
+namespace TestSdk.Http;
+
+/// <summary>
+/// The environments available for the SDK.
+/// </summary>
+public class Environment
+{
+    internal Uri Uri { get; private set; }
+
+    private Environment(string uri)
+    {
+        Uri = new Uri(uri);
+    }
+
+    public static Environment Default { get; } = new("http://petstore.swagger.io/v1/");
+}
